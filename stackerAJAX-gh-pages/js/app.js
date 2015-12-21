f6a05c091f2fa7c17126094a55237c7f57f820f4
Chelsea Yan <changyan@umich.edu>
2015-12-21 01:54:50 -0800

    Reputation Builder

---
+++ stackerAJAX-gh-pages/js/app.js
@@ -0,0 +1,175 @@
+// this function takes the question object returned by the StackOverflow request
+// and returns new result to be appended to DOM
+var showQuestion = function(question) {
+	
+	// clone our result template code
+	var result = $('.templates .question').clone();
+	
+	// Set the question properties in result
+	var questionElem = result.find('.question-text a');
+	questionElem.attr('href', question.link);
+	questionElem.text(question.title);
+
+	// set the date asked property in result
+	var asked = result.find('.asked-date');
+	var date = new Date(1000*question.creation_date);
+	asked.text(date.toString());
+
+	// set the .viewed for question property in result
+	var viewed = result.find('.viewed');
+	viewed.text(question.view_count);
+
+	// set some properties related to asker
+	var asker = result.find('.asker');
+	asker.html('<p>Name: <a target="_blank" '+
+		'href=http://stackoverflow.com/users/' + question.owner.user_id + ' >' +
+		question.owner.display_name +
+		'</a></p>' +
+		'<p>Reputation: ' + question.owner.reputation + '</p>'
+	);
+
+	return result;
+};
+
+var showAnswerer = function(answerer){
+
+	// clone our result template code
+	var result = $('.templates .answerer').clone();
+
+	var img = result.find('img');
+	img.attr('src', answerer.user.profile_image);
+	
+	// Set the answerer properties in result
+	var answererElem = result.find('a');
+	answererElem.attr('href', answerer.user.link);
+
+	// Set user_name
+	var user_name = result.find('h3');
+	user_name.text(answerer.user.display_name);
+
+
+	// set the reputation
+	var reputation = result.find('.reputation');
+	reputation.text(answerer.user.reputation); 
+
+	// set the post-count
+	var post_count = result.find('.post_count');
+	post_count.text(answerer.post_count);
+
+	// set the score
+	var score = result.find('.score');
+	score.text(answerer.score);
+	
+	return result;
+
+}
+
+
+// this function takes the results object from StackOverflow
+// and returns the number of results and tags to be appended to DOM
+var showSearchResults = function(query, resultNum) {
+	var results = resultNum + ' results for <strong>' + query + '</strong>';
+	return results;
+};
+
+// takes error string and turns it into displayable DOM element
+var showError = function(error){
+	var errorElem = $('.templates .error').clone();
+	var errorText = '<p>' + error + '</p>';
+	errorElem.append(errorText);
+};
+
+// takes a string of semi-colon separated tags to be searched
+// for on StackOverflow
+var getUnanswered = function(tags) {
+	
+	// the parameters we need to pass in our request to StackOverflow's API
+	var request = { 
+		tagged: tags,
+		site: 'stackoverflow',
+		order: 'desc',
+		sort: 'creation'
+	};
+	
+	$.ajax({
+		url: "http://api.stackexchange.com/2.2/questions/unanswered",
+		data: request,
+		dataType: "jsonp",//use jsonp to avoid cross origin issues
+		type: "GET",
+	})
+	.done(function(result){ //this waits for the ajax to return with a succesful promise object
+		var searchResults = showSearchResults(request.tagged, result.items.length);
+
+		$('.search-results').html(searchResults);
+		//$.each is a higher order function. It takes an array and a function as an argument.
+		//The function is executed once for each item in the array.
+		$.each(result.items, function(i, item) {
+			var question = showQuestion(item);
+			$('.results').append(question);
+		});
+	})
+	.fail(function(jqXHR, error){ //this waits for the ajax to return with an error promise object
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+};
+
+var getInspiration = function(topic) {
+
+	var request = {
+		tag: topic,
+		site: 'stackoverflow',
+		period: 'all_time'
+	};
+
+	var url_answerers = "http://api.stackexchange.com/2.2/tags/" + topic 
+						+ "/top-answerers/"+ request.period; 
+
+	$.ajax({
+		url: url_answerers,
+		data: request,
+		dataType: "jsonp",//use jsonp to avoid cross origin issues
+		type: "GET",
+	})
+
+	.done(function(result){
+
+		var searchResults = showSearchResults(request.tag, result.items.length);
+
+		$('.search-results').html(searchResults);
+		//$.each is a higher order function. It takes an array and a function as an argument.
+		//The function is executed once for each item in the array.
+		$.each(result.items, function(i, item) {
+			var answerer= showAnswerer(item);
+			$('.results').append(answerer);
+		});
+	})
+
+	.fail(function(jqXHR, error){ //this waits for the ajax to return with an error promise object
+		var errorElem = showError(error);
+		$('.search-results').append(errorElem);
+	});
+}
+
+$(document).ready( function() {
+
+	$('.unanswered-getter').submit( function(e){
+		e.preventDefault();
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the value of the tags the user submitted
+		var tags = $(this).find("input[name='tags']").val();
+		getUnanswered(tags);
+	});
+
+	$('.inspiration-getter').submit( function(e){
+		e.preventDefault();
+		// zero out results if previous search has run
+		$('.results').html('');
+		// get the value of the tags the user submitted
+		var topic = $(this).find("input[name='answerers']").val();
+		getInspiration(topic);
+	})
+
+
+});
